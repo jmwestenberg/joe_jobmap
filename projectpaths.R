c4740b55d3bf453dab4f7eab4f734fa207b4ea7c
Joe Westenberg <jwesten@iu.edu>
2021-11-16 12:07:36 -0500

    added main pathing file

---
+++ projectpaths.R
@@ -0,0 +1,3 @@
+datapath<-file.path(pathtowd, "original_data", fsep = .Platform$file.sep)
+codepath<-file.path(pathtowd, "code", fsep = .Platform$file.sep)
+figurespath<-file.path(pathtowd, "figures", fsep = .Platform$file.sep)
---
+++ runproject.R
@@ -0,0 +1,8 @@
+#Change this to your directory
+pathtowd<-"C:/Users/weste/Documents/GitHub/joe_jobmap"
+
+
+
+source(file.path(pathtowd, "projectpaths.r", fsep = .Platform$file.sep))
+source(file.path(codepath, "staticmap.r", fsep = .Platform$file.sep))
+
